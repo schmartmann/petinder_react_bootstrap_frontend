ffad91e06ba00816fbe67103e3b066f0dfe32785
Stefan Hartmann <stefanhartmann@gmail.com>
2017-04-14 15:22:41 -0400

    update styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,13 +3,9 @@ import React, { Component } from 'react';
 export default class Navbar extends Component {
   render(){
     return(
-      <div>
-        <div className="nav-wrapper">
-          <nav className="nav-items">
-            <img id="paw-icon" src="./style/icons/logo.png"/>
-          </nav>
+      <div className="navbar">
+        <img src="./style/icons/logo.png"/>
       </div>
-    </div>
     )
   }
 }
